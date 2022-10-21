1d4bc711c2e00df11da05108549c85685d607fb3
Edwin Svensson <git@olback.net>
2022-10-21 16:58:24 +0200

    go back to monke

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::FileServer;
+
+#[rocket::main]
+async fn main() {
+    if let Err(e) = rocket::build()
+        .mount("/", FileServer::from("static"))
+        .launch()
+        .await
+    {
+        eprintln!("{e}");
+    }
 }
